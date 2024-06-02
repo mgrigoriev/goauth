161f44fdb3d8442af206c5ada6751dcba17076ca
Mikhail Grigoriev <mgrigoriev@gmail.com>
2024-06-02 19:05:54 +0300

    Add context to main

--- main.go
+++ main.go
@@ -3,6 +3,7 @@
 package main
 
 import (
+	"context"
 	"log"
 )
 import "github.com/mgrigoriev/goauth/authclient"
@@ -13,10 +14,11 @@ const authURL = "http://localhost:8080/api/v1/users/auth"
 func main() {
 	cfg := authclient.Config{AuthURL: authURL}
 	cl := authclient.New(cfg)
+	ctx := context.Background()
 
 	// Loop for testing rate limiter / circuit breaker
 	for {
-		user, err := cl.Authenticate(token)
+		user, err := cl.Authenticate(ctx, token)
 		if err != nil {
 			log.Printf("authentication failed: %v", err)
 		}
